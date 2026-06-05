d208789275ee898b66f9a25b8bfe81977318c1ff
maaztausif <maaztausif48@gmail.com>
2026-06-05 16:58:34 +0200

    Project Created

---
+++ src/main/java/com/maaztausif/khallikarao/KhallikaraoApplication.java
@@ -0,0 +1,13 @@
+package com.maaztausif.khallikarao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhallikaraoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhallikaraoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maaztausif/khallikarao/KhallikaraoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maaztausif.khallikarao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhallikaraoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
